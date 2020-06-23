43eecdc7d9acd6a33b83ae94805640f418ace2bb
Zakaria <info@bigdatahabits.com>
2020-06-23 19:04:11 +0200

    test python

--- R/python.R
+++ R/python.R
@@ -1,11 +1,11 @@
 #' @export
 executepyth <- function (string) {
 
-  fileConn <- file("output.txt")
-  writeLines(string, fileConn)
-  close(fileConn)
+fileConn <- file("output.txt")
+writeLines(string, fileConn)
+close(fileConn)
 
-  return (reticulate::py_run_file("output.txt"))
+reticulate::py_run_file("output.txt")
 
 }
 

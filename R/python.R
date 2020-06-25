76823578e3627eae57a806e5217fe10499085691
Zakaria <info@bigdatahabits.com>
2020-06-25 20:56:14 +0200

    test python

--- R/python.R
+++ R/python.R
@@ -21,8 +21,10 @@ df = pd.DataFrame(data = d)
 print(df)
 print(df.to_csv('out.csv',index= False))"
 
-test1 <- "
-import pandas as np"
+test1 <- '
+import numpy
+a = numpy.asarray([ [1,2,3], [4,5,6], [7,8,9] ])
+numpy.savetxt("foo.csv", a, delimiter=",")'
 
 cat(ocpupyt::executepyth(test1))
 

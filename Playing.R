2ff800d35ee1057cccd7a3b99188d99c13283c22
Zakaria <info@bigdatahabits.com>
2020-06-26 13:06:02 +0200

    test python

--- Playing.R
+++ Playing.R
@@ -6,5 +6,18 @@ print(df)"
 
 ocpupyt::executepyth(test)
 
+test <- "
+import pandas as pd
+d = {'col1': [1, 2], 'col2': [3, 4]}
+df = pd.DataFrame(data = d)
+print(df)
+print(df.to_csv('out.csv',index= False))"
+
+test1 <- '
+import numpy
+a = numpy.asarray([ [1,2,3], [4,5,6], [7,8,9] ])
+numpy.savetxt("foo.csv", a, delimiter=",")'
+
+
 
 
--- R/python.R
+++ R/python.R
@@ -1,32 +1,5 @@
 #' @export
-executepyth <- function (string) {
-
+execute_py <- function (string) {
   library(reticulate)
   py_capture_output(py_run_string(string))
-
-}
-
-#' @export
-executer <- function(string) {
-  cat(string)
-}
-
-
-saveout <- function(string){
-
-test <- "
-import pandas as pd
-d = {'col1': [1, 2], 'col2': [3, 4]}
-df = pd.DataFrame(data = d)
-print(df)
-print(df.to_csv('out.csv',index= False))"
-
-test1 <- '
-import numpy
-a = numpy.asarray([ [1,2,3], [4,5,6], [7,8,9] ])
-numpy.savetxt("foo.csv", a, delimiter=",")'
-
-cat(ocpupyt::executepyth(test1))
-
-
 }
